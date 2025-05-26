a30dc52712413ffe671b3b2aab7d249fb8013163
LeonardoCalder <leoside0808@gmail.com>
2025-05-26 12:37:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ document.getElementById('serviceRequestForm').addEventListener('submit', functio
   console.log('Datos a enviar:', serviceRequestData);
 
   // Enviar la solicitud al backend
-  fetch('https://hl7-fhir-ehr-leonardo.onrender.com/service-requests', {
+  fetch('https://hl7-fhir-ehr-leonardo.onrender.com/service-request', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(serviceRequestData)
